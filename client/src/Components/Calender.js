b56adbbf7224099b4f11880af4fb56b87139eedc
aumcintyre <aumcintyre@gmail.com>
2022-12-22 18:23:56 -0600

    adding renderpage logic to navbar/home page

--- client/src/Components/Calender.js
+++ client/src/Components/Calender.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 
+
+//The number on the left is the current week / 52
+// showWeekNumbers value={value} (add this to the calendar component to show the week numbers again)
+
+
 function ExchangeCalendar() {
   const [value, onChange] = useState(new Date());
 
   return (
-    <div className="Sample">
+    <div>
       <header>
-        <h1>react-calendar sample page</h1>
+        <h1 className='custom-header'>Select your deadline!</h1>
       </header>
-      <div className="Sample__container">
+      <div className="container card">
         <main className="Sample__container__content">
-          <Calendar onChange={onChange} showWeekNumbers value={value} />
+          <Calendar onChange={onChange} />
         </main>
       </div>
     </div>
