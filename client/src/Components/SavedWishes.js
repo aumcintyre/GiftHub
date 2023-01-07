b691e4f3863ea49b70c0ec78256a8e48582cc8e9
Brian Campbell <brian.campbell003@gmail.com>
2023-01-06 21:05:40 -0600

    saved wishes (mostly?) working

--- client/src/Components/SavedWishes.js
+++ client/src/Components/SavedWishes.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_USER } from '../utils/queries';
 
-function SavedWishes(props) {
-    console.log(props);
-    const { data } = useQuery(GET_USER, props.user);
+function SavedWishes({ user }) {
+    console.log(user);
+    const { data } = useQuery(GET_USER, { variables: { username: user }, });
     console.log(data);
+
+    let wishArray = data.user.wishes;
+    console.log(wishArray);
 
 
     return (
         <div className='saved-wishes'>
             <h3>Wishlist</h3>
 
-               {/* {
-               items.map((item, index) => { 
+               {
+               wishArray.map((wish, index) => { 
                 
                 return (
-                    <p key={item}>{item}</p>
-                )})} */}
+                    <p key={index}>{wish}</p>
+                )})}
 
         </div>
     )
